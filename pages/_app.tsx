de0dcfe7aeb5f8f126129de856513db07d13e07a
rivopelu <92528810+rivopelu@users.noreply.github.com>
2022-05-18 03:13:24 +0800

    kedua

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../styles/App.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
